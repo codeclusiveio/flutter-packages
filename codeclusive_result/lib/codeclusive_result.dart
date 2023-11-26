219c15d60330865b0aabe53f61082699de439dab
dszpilski <dominik.szpilski@edgarsz.pl>
2023-11-26 14:08:47 +0100

    initialized codeclusive_result package

---
+++ codeclusive_result/lib/codeclusive_result.dart
@@ -0,0 +1,7 @@
+library codeclusive_result;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ codeclusive_result/test/codeclusive_result_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:codeclusive_result/codeclusive_result.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
