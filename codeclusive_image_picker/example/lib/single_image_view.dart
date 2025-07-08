8816025110656d518dcab69c786fa956a5c17b63
monake1479 <daniel.oblak99@gmail.com>
2025-07-08 09:56:30 +0200

    Bumped versions of packages and flutter

--- codeclusive_image_picker/example/lib/single_image_view.dart
+++ codeclusive_image_picker/example/lib/single_image_view.dart
@@ -4,9 +4,9 @@ import 'package:photo_manager/photo_manager.dart';
 
 class SingleImageView extends StatefulWidget {
   const SingleImageView({
-    Key? key,
+    super.key,
     required this.asset,
-  }) : super(key: key);
+  });
 
   final AssetEntity asset;
 
